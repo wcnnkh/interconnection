302878af5c53fc61f2cb69916468f9ed91796bea
alisa <alisa@172.20.10.10>
2021-09-11 12:04:49 +0800

    rename

--- interconnection-example/src/main/java/io/github/wcnnkh/interconnection/InterconnectionExampleApplication.java
+++ interconnection-example/src/main/java/io/github/wcnnkh/interconnection/InterconnectionExampleApplication.java
@@ -5,9 +5,9 @@ import io.basc.framework.boot.support.MainApplication;
 import io.basc.framework.web.cors.Cors;
 import io.basc.framework.web.cors.CorsRegistry;
 
-public class ExampleApplication {
+public class InterconnectionExampleApplication {
 	public static void main(String[] args) {
-		MainApplication.run(ExampleApplication.class, args);
+		MainApplication.run(InterconnectionExampleApplication.class, args);
 	}
 	
 	@Bean
