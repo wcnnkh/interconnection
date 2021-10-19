5375f009e0e7766bc4761a0a1735e95d6cd31df8
舒超文 <shuchaowen@zx62580.com>
2021-10-19 17:43:43 +0800

    cleanup

---
+++ interconnection-core/src/main/java/io/github/wcnnkh/interconnection/editable/WeixinMpConfig.java
@@ -0,0 +1,11 @@
+package io.github.wcnnkh.interconnection.editable;
+
+import java.io.Serializable;
+
+import io.basc.start.data.annotation.Editable;
+
+@Editable(name = "微信基础配置")
+public class WeixinMpConfig implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+}
