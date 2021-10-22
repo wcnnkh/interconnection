dbc6b0ed2f70e21284c840f4c8e1615dd7db0440
舒超文 <shuchaowen@zx62580.com>
2021-10-22 17:38:57 +0800

    cleanup

--- interconnection-core/src/main/java/io/github/wcnnkh/interconnection/editable/WeixinMpConfig.java
+++ interconnection-core/src/main/java/io/github/wcnnkh/interconnection/editable/WeixinMpConfig.java
@@ -1,14 +1,25 @@
 package io.github.wcnnkh.interconnection.editable;
 
+import io.basc.framework.orm.annotation.PrimaryKey;
 import io.basc.start.data.annotation.Editable;
+import io.basc.start.data.annotation.Readonly;
 
 import java.io.Serializable;
 
 @Editable(title = "微信基础配置")
 public class WeixinMpConfig implements Serializable {
 	private static final long serialVersionUID = 1L;
+	@PrimaryKey
 	private String appid;
 	private String appsecret;
+	
+	//TODO 以下两个token应该分表的，但为了方便所以使用了一张表，使用同一个表会存在并发更新的问题(会增加很多无用的请求)，但这里忽略了
+	private String accessToken;
+	@Readonly
+	private Long accessTokenCreateTime;
+	private String jsApiTicket;
+	@Readonly
+	private Long jsApiTicketCreateTime;
 
 	public String getAppid() {
 		return appid;
@@ -26,4 +37,35 @@ public class WeixinMpConfig implements Serializable {
 		this.appsecret = appsecret;
 	}
 
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public Long getAccessTokenCreateTime() {
+		return accessTokenCreateTime;
+	}
+
+	public void setAccessTokenCreateTime(Long accessTokenCreateTime) {
+		this.accessTokenCreateTime = accessTokenCreateTime;
+	}
+
+	public String getJsApiTicket() {
+		return jsApiTicket;
+	}
+
+	public void setJsApiTicket(String jsApiTicket) {
+		this.jsApiTicket = jsApiTicket;
+	}
+
+	public Long getJsApiTicketCreateTime() {
+		return jsApiTicketCreateTime;
+	}
+
+	public void setJsApiTicketCreateTime(Long jsApiTicketCreateTime) {
+		this.jsApiTicketCreateTime = jsApiTicketCreateTime;
+	}
 }
--- interconnection-web/src/main/java/io/github/wcnnkh/interconnection/web/WeixinController.java
+++ interconnection-web/src/main/java/io/github/wcnnkh/interconnection/web/WeixinController.java
@@ -8,22 +8,31 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 
 import io.basc.framework.beans.annotation.Autowired;
 import io.basc.framework.codec.support.URLCodec;
+import io.basc.framework.context.result.DataResult;
+import io.basc.framework.context.result.ResultFactory;
 import io.basc.framework.logger.Logger;
 import io.basc.framework.logger.LoggerFactory;
 import io.basc.framework.mvc.message.annotation.QueryParams;
 import io.basc.framework.mvc.model.ModelAndView;
 import io.basc.framework.net.uri.UriUtils;
+import io.basc.framework.oauth2.AccessToken;
+import io.basc.framework.security.Token;
 import io.basc.framework.util.StringUtils;
 import io.basc.framework.web.ServerHttpRequest;
 import io.basc.framework.web.ServerHttpResponse;
 import io.basc.start.app.configure.AppConfigure;
 import io.basc.start.data.DataService;
+import io.basc.start.tencent.wx.JsApiSignature;
+import io.basc.start.tencent.wx.WeiXinUtils;
+import io.github.wcnnkh.interconnection.editable.WeixinMpConfig;
 import io.github.wcnnkh.interconnection.editable.WxConnectConfig;
 import io.github.wcnnkh.interconnection.web.dto.WxAuthorizeRequest;
 import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -34,6 +43,8 @@ public class WeixinController {
 	private final DataService dataService;
 	@Autowired
 	private AppConfigure appConfigure;
+	@Autowired
+	private ResultFactory resultFactory;
 
 	public WeixinController(DataService dataService) {
 		this.dataService = dataService;
@@ -88,5 +99,83 @@ public class WeixinController {
 			logger.debug("connect [{}] to [{}]", connectId, url);
 		}
 		return view;
+	}
+	
+	@Path("/get/token")
+	@GET
+	@Operation(description = "获取AccessToken")
+	public DataResult<String> getAccessToken(@QueryParam("appid") String appid) {
+		WeixinMpConfig config = dataService.getById(WeixinMpConfig.class, appid);
+		if(config == null) {
+			return resultFactory.error("appid不存在");
+		}
+
+		Long createTime = config.getAccessTokenCreateTime();
+		if(StringUtils.isEmpty(config.getAccessToken()) || createTime == null || (System.currentTimeMillis() - createTime) > (7200 - 60) * 1000) {
+			//已过期
+			synchronized (this) {
+				config = dataService.getById(WeixinMpConfig.class, appid);
+				if(config == null) {
+					return resultFactory.error("appid不存在");
+				}
+				
+				createTime = config.getAccessTokenCreateTime();
+				if(StringUtils.isEmpty(config.getAccessToken()) || createTime == null || (System.currentTimeMillis() - createTime) > (7200 - 60) * 1000) {
+					AccessToken accessToken = WeiXinUtils.getAccessToken(config.getAppid(), config.getAppsecret());
+					config.setAccessToken(accessToken.getToken().getToken());
+					config.setAccessTokenCreateTime(System.currentTimeMillis());
+					dataService.update(config);
+				}
+			}
+		}
+		return resultFactory.success(config.getAccessToken());
+	}
+	
+	@Path("/get/ticket")
+	@GET
+	@Operation(description = "获取js api ticket")
+	public DataResult<String> getJsApiTicket(@QueryParam("appid") String appid){
+		WeixinMpConfig config = dataService.getById(WeixinMpConfig.class, appid);
+		if(config == null) {
+			return resultFactory.error("appid不存在");
+		}
+		
+		Long createTime = config.getJsApiTicketCreateTime();
+		if(StringUtils.isEmpty(config.getJsApiTicket()) || createTime == null || (System.currentTimeMillis() - createTime) > (7200 - 60) * 1000) {
+			//已过期
+			synchronized (this) {
+				config = dataService.getById(WeixinMpConfig.class, appid);
+				if(config == null) {
+					return resultFactory.error("appid不存在");
+				}
+				
+				DataResult<String> accessTokenResponse = getAccessToken(appid);
+				if(accessTokenResponse.isError()) {
+					return accessTokenResponse;
+				}
+				
+				createTime = config.getJsApiTicketCreateTime();
+				if(StringUtils.isEmpty(config.getJsApiTicket()) || createTime == null || (System.currentTimeMillis() - createTime) > (7200 - 60) * 1000) {
+					Token token = WeiXinUtils.getJsApiTicket(accessTokenResponse.getData());
+					config.setJsApiTicket(token.getToken());
+					config.setJsApiTicketCreateTime(System.currentTimeMillis());
+					dataService.update(config);
+				}
+			}
+		}
+		return resultFactory.success(config.getJsApiTicket());
+	}
+	
+	@Path("/get/signature")
+	@GET
+	@Operation(description = "获取js api signature")
+	public DataResult<JsApiSignature> getJsApiSignature(@QueryParam("appid") String appid, @QueryParam("url") String url){
+		DataResult<String> ticketResponse = getJsApiTicket(appid);
+		if(ticketResponse.isError()) {
+			return ticketResponse.dataResult();
+		}
+		
+		JsApiSignature jsApiSignature = new JsApiSignature(ticketResponse.getData(), url);
+		return resultFactory.success(jsApiSignature);
 	}
 }
