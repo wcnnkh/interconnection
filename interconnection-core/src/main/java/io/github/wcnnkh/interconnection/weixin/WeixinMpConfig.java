28e579c6073d116781fcac9241c1eabee31e3088
wcnnkh <247044007@qq.com>
2021-10-24 22:13:12 +0800

    cleanup

--- interconnection-core/src/main/java/io/github/wcnnkh/interconnection/weixin/WeixinMpConfig.java
+++ interconnection-core/src/main/java/io/github/wcnnkh/interconnection/weixin/WeixinMpConfig.java
@@ -1,5 +1,6 @@
 package io.github.wcnnkh.interconnection.weixin;
 
+import io.basc.framework.lang.Nullable;
 import io.basc.framework.orm.annotation.PrimaryKey;
 import io.basc.start.data.annotation.Editable;
 import io.basc.start.data.annotation.Readonly;
@@ -11,6 +12,7 @@ public class WeixinMpConfig implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@PrimaryKey
 	private String appid;
+	@Nullable(false)
 	private String appsecret;
 	
 	//TODO 以下两个token应该分表的，但为了方便所以使用了一张表，使用同一个表会存在并发更新的问题(会增加很多无用的请求)，但这里忽略了
